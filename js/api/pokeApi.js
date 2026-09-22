e845690b000bf4e55603d977d77db8db1573239e
Alain <alain.rodhain@edu.devinci.fr>
2026-09-22 15:18:17 +0200

    barre de recherche qui marche normalement

--- js/api/pokeApi.js
+++ js/api/pokeApi.js
@@ -1,3 +1,6 @@
+
+import { setupSearchBar } from "../searchBar.js";
+
 const getFrenchName = (species, fallback) => {
     for (let i = 0; i < species.names.length; i++) {
         if (species.names[i].language.name === "fr") {
@@ -54,6 +57,7 @@ export const getPokemonList = async () => {
 
             content += `
                 <div class="carte" data-id="${pokemon.id}">
+                    <button class="fav"></button>
                     <h2>${pokemon.name}</h2>
                     <img src="${pokemon.sprite}" alt="${pokemon.name}">
                 </div>
@@ -64,6 +68,8 @@ export const getPokemonList = async () => {
         elements.className = 'grille'
         elements.innerHTML = content
         document.body.appendChild(elements)
+
+        setupSearchBar()
     } else {
         displayError("Une erreur est survenue")
     }
